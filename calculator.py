977aa803d049dd177a0eda30dc03fd3bb5b805c1
13830269168 <13830269168@163.com>
2017-10-22 13:07:28 +0800

    second calculator

--- calculator.py
+++ calculator.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
 import sys
-try:
-    a = int(sys.argv[1]) - int(3500)
-except(ValueError,IndexError):
-    print("Parameter Error")
-else:
-    if a <= 0:
-        b = 0
-    elif a <= 1500 and a > 0:
-        b = a * 0.03 - 0
-    elif a > 1500 and a <= 4500:
-        b = a * 0.1 - 105
-    elif a > 4500 and a <= 9000:
-        b = a * 0.2 - 555
-    elif a > 9000 and a <= 35000:
-        b = a * 0.25 - 1005
-    elif a > 35000 and a <= 55000:
-        b = a * 0.3 - 2755
-    elif a > 55000 and a <= 80000:
-        b = a * 0.35 - 5505
+def result(argv):
+    for s in sys.argv[1:]:
+        try :
+            number,salary = s.split(':')
+            salary = int(salary)
+        except(IndexError,ValueError):
+            print("Parameter Error")
+        else:
+            a = salary - 3500 - suf(salary)
+            incometax(a)
+            print(number+':'+format(salary - incometax(a) - suf(salary),".2f"))
+def suf(salary):
+    value = salary * 0.165
+    return value
+def incometax(ratable):
+    if ratable <= 0:
+        tax = 0
+    elif ratable < 1500:
+        tax = ratable * 0.03
+    elif ratable > 1500 and ratable <= 4500:
+        tax = ratable * 0.1 - 105
+    elif ratable > 4500 and ratable <= 9000:
+        tax = ratable * 0.2 - 555
+    elif ratable > 9000 and ratable <= 35000:
+        tax = ratable * 0.25 - 1005
+    elif ratable > 35000 and ratable <= 55000:
+        tax = ratable * 0.3 - 2755
+    elif ratable > 55000 and ratable <= 80000:
+        tax = ratable * 0.35 - 5505
     else:
-        b = a * 0.45 - 13505
-    print(format(b,".2f"))
+        tax = ratable * 0.45 - 13505
+    return tax
+if __name__ == '__main__':
+    result(sys.argv[1:])
